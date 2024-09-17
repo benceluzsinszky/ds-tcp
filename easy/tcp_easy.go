98faf43ccc91a04af23ba05364a9da5ccfcb6648
bence <benceluzsinszky@gmail.com>
2024-09-17 21:20:55 +0200

    refactor

--- easy/tcp_easy.go
+++ easy/tcp_easy.go
@@ -1,82 +1,56 @@
-package easy
+package main
 
 import (
 	"fmt"
 	"time"
 )
 
-func easyClient(syn chan int, ack chan int) {
-	syn_x := 100
+func client(syn chan int, ack chan int) {
+	seq_client := 100
 
-	//1.sends syn x
-	syn <- syn_x
-	fmt.Println("Client sends syn x")
+	syn <- seq_client
+	fmt.Println("Client sends SYN")
+	ack_server := <-ack
+	seq_server := <-syn
+	fmt.Println("Client recieves server's SYN and ACK")
 
-	//4. recieves syn y and ack_y
-
-	ack_y := <-ack
-	syn_y := <-syn
-	fmt.Println("Client recieves server's syn y and ack_y")
-
-	//5. validate ack
-	fmt.Println("Client validates server s ack")
-	if ack_y != syn_x+1 {
-		fmt.Println("Invalid ack")
+	fmt.Println("Client validates server's ACK")
+	if ack_server != seq_client+1 {
+		fmt.Println("Invalid ACK")
 		return
 	}
-	fmt.Println("Client sends ack and syn")
-
-	//6. sends ack = y +1, sends syn x + 1
-	ack <- syn_y + 1
-	syn <- syn_x + 1
-
+	fmt.Println("Client sends ACK")
+	ack <- seq_server + 1
+	syn <- seq_client + 1
 }
 
-func easyServer(syn chan int, ack chan int) {
-	syn_y := 300
+func server(syn chan int, ack chan int) {
+	seq_server := 300
 
-	//2.receives syn x
-	syn_x := <-syn
-	fmt.Println("Server receives client s syn ")
-
-	//3.sends ack = x +1 , syn = y
-	syn <- syn_y
-
-	ack <- syn_x + 1
-	fmt.Println("Server sends ack = x +1 , syn = y")
-
-	//7. receive the ack from client
-	ack_x := <-ack
+	seq_client := <-syn
+	fmt.Println("Server receives client's SYN")
+	syn <- seq_server
+	ack <- seq_client + 1
+	fmt.Println("Server sends SYN and ACK")
+	ack_client := <-ack
 	seq := <-syn
-	fmt.Println("Server receives client s ack from client")
 
-	//8. validate ack
-	fmt.Println("Server validates client s ack and seq")
-
-	if ack_x != syn_y+1 || seq != syn_x+1 {
+	fmt.Println("Server receives client's ACK")
+	fmt.Println("Server validates client's ACK")
+	if ack_client != seq_server+1 || seq != seq_client+1 {
 		fmt.Println("Invalid ack")
 		return
 	}
-
 	fmt.Println("Connection estabilished")
 
 }
 
-func checkChannel(signal chan bool) {
-	for {
-		// Wait for a signal from a philosopher to request or release the fork
-		signal <- true // Fork is available (this is the signal that a philosopher can take the fork)
-		<-signal       // Wait for a signal from a philosopher to release the fork
-	}
-}
-
 func main() {
-
 	syn := make(chan int, 1)
 	ack := make(chan int, 1)
 
-	go easyClient(syn, ack)
-	go easyServer(syn, ack)
+	go client(syn, ack)
+	go server(syn, ack)
 
 	time.Sleep(30 * time.Second)
 }
